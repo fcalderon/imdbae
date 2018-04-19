8fb2e01729b0a2105560a0dff2fbee9fe42cad31
Francisco J Calderon <fjavier5152@gmail.com>
2018-04-18 21:11:25 -0400

    #11 - Load and show movies (initial) (#14)
    * #11 - Load last 10 movies
    
    * #11 - show movies

--- assets/js/imdbae/movies/movies-page.jsx
+++ assets/js/imdbae/movies/movies-page.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import {MovieListItem} from "./movie-list-item";
+
 
 export const Movies = (props) => {
-  return (<h1>Movies</h1>);
+  return (<div>
+    <h1>Movies</h1>
+    {renderMovies(props.movies)}
+  </div>);
 };
+
+function renderMovies(movies) {
+  const renderedMovies = [];
+
+  for (let i = 0; i < movies.length; i++) {
+    renderedMovies.push(renderMovie(movies[i]));
+  }
+
+  return <div>
+    {renderedMovies}
+  </div>
+}
+
+function renderMovie(movie) {
+  return <MovieListItem key={movie.id} movie={movie}></MovieListItem>
+}
