dcb0dd1d8b85aaa6618d776f99d9d23163f521be
Yasmeen <yemampoor@ccs.neu.edu>
2018-04-19 20:11:04 -0400

    added alt text

--- assets/js/imdbae/layout/nav.jsx
+++ assets/js/imdbae/layout/nav.jsx
@@ -17,7 +17,7 @@ const mapDispatchToProps = dispatch => {
 
 export const Nav = connect(mapStateToProps, mapDispatchToProps)((props) => {
   return (<nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "#FFC0CB"}}>
-    <a className={'navbar-brand'} href="/"><img src="/images/imdbae.png"/></a>
+    <a className={'navbar-brand'} href="/"><img src="/images/imdbae.png" alt="IMDbae"/></a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"/>
