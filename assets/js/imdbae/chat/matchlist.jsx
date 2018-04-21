e839f3eef4b934124f04f7c707940ed7e938beb7
Yasmeen <yemampoor@ccs.neu.edu>
2018-04-21 02:13:12 -0400

    generating pair id

--- assets/js/imdbae/chat/matchlist.jsx
+++ assets/js/imdbae/chat/matchlist.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import socket from '../../socket';
+import _ from 'lodash';
+
 
 const mapStateToProps = (state) => {
     return {
-        //IDK
+		matches: state.matches.matches.data,
     }
 }
 
@@ -14,21 +16,51 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
+
 class Matches extends React.Component {
     constructor(props) {
         super(props);
         this.state = {value: ""};
     }
 
+	processMatches(matches) {
+		let proc =  _.chain(matches)
+		.groupBy(m => m.second_user_id)
+		.map((value, key) => ({first_user: value[0].first_user, second_user: value[0].second_user}))
+		.value();
+		return proc;
+	}
+
     render() {
         return (<div className={'card chat-room'}>
              <div className={'card-header'}>
-               YourMatches
+               YourMatches  
              </div>
              <div className={'card-body card-users'} id={'your-matches'}>
-             </div>
+				{this.processMatches(this.props.matches).map((match, index) => <MatchButton props={match} key={match.second_user_id}/>)}
+			</div>
            </div>);
     }
+}
+
+class MatchButton extends React.Component {
+	constructor(props) {
+		super(props);
+		this.second_user = props.props.second_user;
+		this.combined_id = this.combinedIds(props.props.first_user, props.props.second_user);
+	}
+	render () {
+		return <div><button className={'btn btn-block'} style={{margin: '3px'}}>{this.second_user.name}</button></div>
+	}
+
+	combinedIds(first_user, second_user) {
+		let id1 = first_user.id;
+		let id2 = second_user.id;
+		let arr = [id1, id2];
+		arr.sort();
+		let new_id = arr.join('');
+		return new_id;
+	}
 }
 
 export const Matchlist = connect(mapStateToProps, mapDispatchToProps)(Matches);
