c56022f71e4ef8bd302d4e0bff8ea81e9721182d
Emily D <emdaniel@ccs.neu.edu>
2018-04-21 01:09:14 -0400

    started match connections

--- assets/js/imdbae/chat/matchlist.jsx
+++ assets/js/imdbae/chat/matchlist.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import socket from '../../socket';
 
-export const Matchlist = () => {
-    return <div className={'card chat-room'}>
+const mapStateToProps = (state) => {
+    return {
+        //IDK
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        //???
+    }
+}
+
+class Matches extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ""};
+    }
+
+    render() {
+        return (<div className={'card chat-room'}>
              <div className={'card-header'}>
                YourMatches
              </div>
              <div className={'card-body card-users'} id={'your-matches'}>
              </div>
-           </div>
-};
+           </div>);
+    }
+}
+
+export const Matchlist = connect(mapStateToProps, mapDispatchToProps)(Matches);
