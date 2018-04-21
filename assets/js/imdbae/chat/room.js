c3a52cc7389e17030fc3ea0fdf33d20ba51f5a12
Emily D <emdaniel@ccs.neu.edu>
2018-04-20 23:55:53 -0400

    fixed typo

--- assets/js/imdbae/chat/room.js
+++ assets/js/imdbae/chat/room.js
@@ -31,7 +31,7 @@ export default function (state = initialState, action) {
 			return {
 				...state,
 				messages: [
-					...state.message, 
+					...state.messages, 
 					action.message, 
 				],
 			};
