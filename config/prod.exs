4a4882a5c492fd29567849733749ce33f3c1d712
Francisco Calderon <fjavier5152@gmail.com>
2018-04-22 01:57:18 -0400

    release - enable server in prod

--- config/prod.exs
+++ config/prod.exs
@@ -56,7 +56,7 @@ config :logger, level: :info
 # Alternatively, you can configure exactly which server to
 # start per endpoint:
 #
-#     config :imdbae, ImdbaeWeb.Endpoint, server: true
+config :imdbae, ImdbaeWeb.Endpoint, server: true
 #
 
 # Finally import the config/prod.secret.exs
